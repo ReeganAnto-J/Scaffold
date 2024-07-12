8cfae06b7283d4a5653c6b06e5b61289a7b44516
reegananto <reegananto>
2024-07-12 11:22:53 +0530

    added java_scaffold design

--- Makefile
+++ Makefile
@@ -1,20 +1,27 @@
+# To build the debug version of the software
 build:
-	@gcc -O0 -Wall ./src/main.c -o ./bin/debug/snippet
+	@gcc -O0 -Wall ./src/main.c -o ./bin/debug/snippet.out
 
+# To build the release version of the software
 buildr:
 	@gcc -Os ./src/main.c -o ./bin/release/snippet
 
+# To run the debug version of the software
 run: build
-	@./bin/debug/snippet
+	@./bin/debug/snippet.out
 
+# To run the release version of the software
 runr: buildr
 	@./bin/release/snippet
 
+# To clean the debug builds
 cleanb:
 	@rm ./bin/debug/snippet
 
+# To clean the release builds
 cleanr:
 	@rm ./bin/release/snippet
 
+# To clean both builds
 clean: cleanb cleanr
 
--- src/main.c
+++ src/main.c
@@ -20,15 +20,71 @@ main.c:
 #include <unistd.h>
 #include <string.h>
 
-const int MAX_PATH = 255;
-
 int getCurretWorkingDir();
+int java_scaffold(const char* const);
+
+const int MAX_PATH = 255;
 
 int main(){
     // To get the directory on which the program is running on
     char currentDirectory[MAX_PATH];
     int err = getCurretWorkingDir(currentDirectory);
-    if(err) exit(1);
+    if(err) {
+        printf("Please run this program on a directory with shorter length\n");
+        printf("Limit 255!");
+        exit(1);
+    }
+
+    // To choose what type of scaffolding the user needs
+    int ch;
+    printf("1. Java\n");
+    scanf("%d",&ch);
+    switch(ch){
+        case 1:
+            err = java_scaffold(currentDirectory);
+            printf("\033[0m");
+            if(err) exit(1);
+            break;
+    }
+}
+
+int java_scaffold(const char* const){
+    FILE *fp;
+
+    // To print a cool java ascii script
+    {
+        // Ref: http://www.patorjk.com/software/taag/#p=display&f=Impossible&t=JAVA
+        printf("\033[32;1;5m");
+        printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+        printf("|             _        _      _          _       _          |\n");
+        printf("|            /\\ \\     / /\\   /\\ \\    _ / /\\     / /\\        |\n");
+        printf("|            \\ \\ \\   / /  \\  \\ \\ \\  /_/ / /    / /  \\       |\n");
+        printf("|            /\\ \\_\\ / / /\\ \\  \\ \\ \\ \\___\\/    / / /\\ \\      |\n");
+        printf("|           / /\\/_// / /\\ \\ \\ / / /  \\ \\ \\   / / /\\ \\ \\     |\n");
+        printf("|  _       / / /  / / /  \\ \\ \\\\ \\ \\   \\_\\ \\ / / /  \\ \\ \\    |\n");
+        printf("| /\\ \\    / / /  / / /___/ /\\ \\\\ \\ \\  / / // / /___/ /\\ \\   |\n");
+        printf("| \\ \\_\\  / / /  / / /_____/ /\\ \\\\ \\ \\/ / // / /_____/ /\\ \\  |\n");
+        printf("| / / /_/ / /  / /_________/\\ \\ \\\\ \\ \\/ // /_________/\\ \\ \\ |\n");
+        printf("|/ / /__\\/ /  / / /_       __\\ \\_\\\\ \\  // / /_       __\\ \\_\\|\n");
+        printf("|\\/_______/   \\_\\___\\     /____/_/ \\_\\/ \\_\\___\\     /____/_/|\n");
+        printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+        printf("\033[0m");
+    }
+    // To check if jdk exists
+    printf("Testing for JDK in path: ");
+    fp = popen("java --version", "r");
+    if(fp == NULL){
+        printf("Cannot find java path!\nExiting...");
+        return 1;
+    }
+    char jdk_path[MAX_PATH];
+    fgets(jdk_path, MAX_PATH, fp);
+    printf("%s\n",jdk_path);
+    pclose(fp);
+    printf("\033[32;1m");
+    printf("\aJDK available ┗(^o^ )┓三\n\n");
+    printf("\033[0m");
+    return 0;
 }
 
 int getCurretWorkingDir(char* pwd){
